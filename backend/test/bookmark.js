f4703f6d713e915bd841cbadee75cff294dd5dc3
Pedro Correa <pedroorez@gmail.com>
2017-03-28 19:16:13 -0300

    Adds test for GET bookmarks

--- backend/test/bookmark.js
+++ backend/test/bookmark.js
@@ -12,6 +12,7 @@ const User = require('../src/database/user.js');
 const Bookmark = require('../src/database/bookmark.js');
 const Config = require('../src/config.js');
 const jwt = require('jsonwebtoken');
+const Promise = require('bluebird');
 
 beforeEach('Deleting Test Database', () => {
   return User.remove({});
@@ -83,6 +84,50 @@ describe('DELETE /bookmark', () => {
           .then((response) => {
             assert.equal(response, null);
           });
+      });
+  });
+});
+
+describe('GET /bookmark', () => {
+  beforeEach((done) => {
+    const passwordHash = bcrypt.hashSync('user', 10);
+
+    User.remove({})
+      .then(() => Bookmark.remove({}))
+      .then(() => Promise.all([
+        User.create({ username: 'admin', password: passwordHash, admin: true }),
+        User.create({ username: 'user', password: passwordHash, admin: false }),
+      ]))
+      .spread((admin, user) => Promise.all([
+        Bookmark.create({ title: 'Page1', url: 'http://old.com', owner: admin }),
+        Bookmark.create({ title: 'Page2', url: 'http://old.com', owner: user }),
+        Bookmark.create({ title: 'Page3', url: 'http://old.com', owner: user }),
+        Bookmark.create({ title: 'page4', url: 'http://old.com', owner: user }),
+      ]))
+      .then(() => done());
+  });
+
+  it('Get Bookmarks as Admin User', () => {
+    const JWT = jwt.sign({ username: 'admin' }, Config.JWT_SECRET_KEY);
+    return request(app)
+      .get('/bookmark/')
+      .set('Authorization', `Bearer ${JWT}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        assert.equal(response.body.length, 4);
+      });
+  });
+
+  it('Get Bookmarks as Normal User', () => {
+    const JWT = jwt.sign({ username: 'user' }, Config.JWT_SECRET_KEY);
+    return request(app)
+      .get('/bookmark/')
+      .set('Authorization', `Bearer ${JWT}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        assert.equal(response.body.length, 3);
       });
   });
 });

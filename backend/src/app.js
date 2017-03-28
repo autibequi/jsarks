eac87093faf4c7df0d964b9cdacaceb706801cf3
Pedro Correa <pedroorez@gmail.com>
2017-03-28 15:21:56 -0300

    Adds JWT validation.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,23 @@
 // Modules
 const app = require('express')();
+const jwt = require('express-jwt');
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 
 // Custom Modules
 const userRouter = require('./routers/user.js');
 const loginRouter = require('./routers/login.js');
+const Config = require('./config.js');
+
+// JWT Setup
+const jwtMiddleware = jwt({ secret: Config.JWT_SECRET_KEY })
+                        .unless({ path: ['/user', '/login'] });
 
 // Setup
 app.use(expressValidator());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use(jwtMiddleware);
 
 // Routers
 app.use(userRouter);

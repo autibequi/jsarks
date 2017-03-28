d6c60ca262134cf37cbbd71fc6be4c5be93793c1
Pedro Correa <pedroorez@gmail.com>
2017-03-28 18:23:51 -0300

    Adds PUT /bookmark implementation

--- backend/src/routers/bookmark.js
+++ backend/src/routers/bookmark.js
@@ -29,5 +29,31 @@ router.post('/bookmark', (req, res) => {
     .catch(err => res.status(err.status ? err.status : 500).send({ err }));
 });
 
+// Update Bookmark
+router.put('/bookmark/:id', (req, res) => {
+  req.checkBody('url', 'title', 'Must not be empty').notEmpty();
+  req.checkBody('title', 'Must only contains lowercase alpha numeric').isAlpha();
+  req.checkBody('url', 'Must only contains lowercase alpha numeric').isURL();
+
+  const title = req.body.title;
+  const url = req.body.url;
+  const id = req.params.id;
+  const JWTData = jwt.verify(Utils.getToken(req), Config.JWT_SECRET_KEY);
+
+  req.getValidationResult()
+    .then(Utils.checkValidations)
+    .then(() => User.findOne({ username: JWTData.username }))
+    .then(owner => Bookmark.findOne({ _id: id, owner }))
+    .then((bookmark) => {
+      if (!bookmark) {
+        return Promise.reject({ status: 404, error: 'Bookmark not found.', id });
+      }
+      return bookmark;
+    })
+    .then(() => Bookmark.findByIdAndUpdate(id, { title, url }, { new: true }))
+    .then(bookmark => res.status(200).send({ title: bookmark.title, url: bookmark.url }))
+    .catch(err => res.status(err.status ? err.status : 500).send({ err }));
+});
+
 // Export Router
 module.exports = router;

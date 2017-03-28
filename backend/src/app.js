70ea928bafab11a5576805aa4bda19046c652164
Pedro Correa <pedroorez@gmail.com>
2017-03-28 10:11:39 -0300

    Adds express-validator middleware.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,11 @@
 // Modules
 const app = require('express')();
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+// Setup
+app.use(expressValidator());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 // Routers
 app.use(userRouter);

d44c75177d25d85d5918008e522a17882eacd08b
Pedro Correa <pedroorez@gmail.com>
2017-03-28 10:08:21 -0300

    Adds user model.

---
+++ backend/src/database/user.js
@@ -0,0 +1,21 @@
+// Custom Modules
+const mongoose = require('./db.js');
+
+// Define ShortURl Model
+module.exports = mongoose.model('User',
+  {
+    username: {
+      type: String,
+      unique: true,
+      dropDups: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    admin: {
+      type: Boolean,
+      required: true,
+    },
+  });

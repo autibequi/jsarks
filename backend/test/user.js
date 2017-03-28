92b050aa6d47f016b61b78a23d3b67a7ee26b287
Pedro Correa <pedroorez@gmail.com>
2017-03-28 16:46:53 -0300

    Adds Test for getting all users.

--- backend/test/user.js
+++ backend/test/user.js
@@ -8,6 +8,8 @@ const request = require('supertest');
 const app = require('../src/app.js');
 const assert = require('assert');
 const User = require('../src/database/user.js');
+const jwt = require('jsonwebtoken');
+const Config = require('../src/config.js');
 
 beforeEach('Deleting Test Database', () => User.remove({}));
 
@@ -31,5 +33,32 @@ describe('POST /user', () => {
       .send({ username: 'admin', password: 'admin', admin: true })
       .expect('Content-Type', /json/)
       .expect(409);
+  });
+});
+
+describe('GET /user', () => {
+  it('Get all users if admin', () => {
+    const JWT = jwt.sign({ username: 'admin' }, Config.JWT_SECRET_KEY);
+    return Promise.all([
+      User.create({ username: 'admin', password: 'admin', admin: true }),
+      User.create({ username: 'user', password: 'admin', admin: false }),
+      User.create({ username: 'user2', password: 'admin', admin: false }),
+    ])
+    .then(() => {
+      return request(app)
+        .get('/user')
+        .set('Authorization', `Bearer ${JWT}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          assert.equal(response.body.length, 3);
+        });
+    });
+  });
+
+  it('Deny Access if not admin', () => {
+    return request(app)
+      .get('/user')
+      .expect(401);
   });
 });

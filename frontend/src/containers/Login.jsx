dc39ecc4d11b5a87de06875e12de7a14811aa002
Pedro Correa <pedroorez@gmail.com>
2017-03-29 19:39:41 -0300

    Adds error messages to Login page.

--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -1,20 +1,21 @@
 /* global document */
 // Libs
 import React from 'react';
-import FormData from 'form-data';
-import { Form, Button, FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap';
+import { Panel, Alert, Form, Button, FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap';
 import ApiService from '../modules/API.jsx';
 import Auth from '../modules/Auth.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
+  getInitialState() {
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
+    return {
+      email: '',
+      password: '',
+      showAlert: false,
+      alertLevel: '',
+      alertMessage: '',
+    };
   }
 
   handleChange(e) {
@@ -24,61 +25,75 @@ class App extends React.Component {
   }
 
   signup(e) {
+    e.preventDefault();
+
     const form = {
       username: this.state.email,
       password: this.state.password,
       admin: false,
     };
 
+    const that = this;
     ApiService.Login(form)
       .then((response) => {
-        Auth.authenticateUser(response.data.token)
+        Auth.authenticateUser(response.data.token);
+        this.context.router.push('/bookmarks');
+        that.setState({ alertLevel: 'success', showAlert: false, alertMessage: 'Correct Password, Loging in.' });
       })
-      .catch((a) => {
+      .catch(() => {
+        that.setState({ alertLevel: 'warning', showAlert: true, alertMessage: 'Wrong User/Password' });
       });
   }
 
   render() {
     return (
-      <Form horizontal>
-        <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={2}>
-           Email
-          </Col>
-          <Col sm={10}>
-            <FormControl
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={this.handleChange}
-              value={this.state.email}
-            />
-          </Col>
-        </FormGroup>
+      <div>
+        <Panel collapsible expanded={this.state.showAlert} bsClass>
+          <Alert bsStyle={this.state.alertLevel}>
+            {this.state.alertMessage}
+          </Alert>
+        </Panel>
 
-        <FormGroup controlId="formHorizontalPassword">
-          <Col componentClass={ControlLabel} sm={2}>
-           Password
-          </Col>
-          <Col sm={10}>
-            <FormControl
-              type="password"
-              name="password"
-              placeholder="Password"
-              onChange={this.handleChange}
-              value={this.state.password}
-            />
-          </Col>
-        </FormGroup>
+        <Form horizontal onSubmit={this.signup}>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+             Email
+            </Col>
+            <Col sm={10}>
+              <FormControl
+                type="text"
+                placeholder="Username"
+                name="email"
+                onChange={this.handleChange}
+                value={this.state.email}
+              />
+            </Col>
+          </FormGroup>
 
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Button onClick={this.signup}>
-             Login
-           </Button>
-          </Col>
-        </FormGroup>
-      </Form>
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+             Password
+            </Col>
+            <Col sm={10}>
+              <FormControl
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={this.state.password}
+              />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit">
+               Login
+             </Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
     );
   }
 }

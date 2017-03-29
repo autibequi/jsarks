5b9d5c91809c231b32a0e7b68a03a373413c49d4
Pedro Correa <pedroorez@gmail.com>
2017-03-29 18:02:58 -0300

    Adds login container.

---
+++ frontend/src/containers/Login.jsx
@@ -0,0 +1,86 @@
+/* global document */
+// Libs
+import React from 'react';
+import FormData from 'form-data';
+import { Form, Button, FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap';
+import ApiService from '../modules/API.jsx';
+import Auth from '../modules/Auth.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
+  }
+
+  handleChange(e) {
+    const dict = {};
+    dict[e.target.name] = e.target.value;
+    this.setState(dict);
+  }
+
+  signup(e) {
+    const form = {
+      username: this.state.email,
+      password: this.state.password,
+      admin: false,
+    };
+
+    ApiService.Login(form)
+      .then((response) => {
+        Auth.authenticateUser(response.data.token)
+      })
+      .catch((a) => {
+      });
+  }
+
+  render() {
+    return (
+      <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+           Email
+          </Col>
+          <Col sm={10}>
+            <FormControl
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+           Password
+          </Col>
+          <Col sm={10}>
+            <FormControl
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button onClick={this.signup}>
+             Login
+           </Button>
+          </Col>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+export default App;

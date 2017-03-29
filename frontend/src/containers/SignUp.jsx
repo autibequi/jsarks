e897816e1f08d4eb0acc6719bdb2f5856664f9a8
Pedro Correa <pedroorez@gmail.com>
2017-03-29 19:30:13 -0300

    Adds error message to SignUp Page.

--- frontend/src/containers/SignUp.jsx
+++ frontend/src/containers/SignUp.jsx
@@ -1,19 +1,20 @@
 /* global document */
 // Libs
 import React from 'react';
-import FormData from 'form-data';
-import { Form, Button, FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap';
+import { Panel, Alert, Form, Button, FormGroup, FormControl, Col, ControlLabel } from 'react-bootstrap';
 import ApiService from '../modules/API.jsx';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
+  getInitialState() {
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
+    return {
+      email: '',
+      password: '',
+      showAlert: false,
+      alertLevel: '',
+      alertMessage: '',
+    };
   }
 
   handleChange(e) {
@@ -23,62 +24,74 @@ class App extends React.Component {
   }
 
   signup(e) {
+    e.preventDefault();
+
     const form = {
       username: this.state.email,
       password: this.state.password,
       admin: true,
     };
-
+    const that = this;
     ApiService.SignUp(form)
-      .then((a) => {
-        debugger;
+      .then(() => {
+        that.setState({ alertLevel: 'success', showAlert: true, alertMessage: 'User Created' });
       })
-      .catch((a) => {
-        debugger;
+      .catch(() => {
+        that.setState({ alertLevel: 'warning', showAlert: true, alertMessage: 'Login Already Exists' });
       });
   }
 
   render() {
     return (
-      <Form horizontal>
-        <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={2}>
-           Email
-          </Col>
-          <Col sm={10}>
-            <FormControl
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={this.handleChange}
-              value={this.state.email}
-            />
-          </Col>
-        </FormGroup>
+      <div>
+        <Panel collapsible expanded={this.state.showAlert} bsClass>
+          <Alert bsStyle={this.state.alertLevel}>
+            {this.state.alertMessage}
+          </Alert>
+        </Panel>
 
-        <FormGroup controlId="formHorizontalPassword">
-          <Col componentClass={ControlLabel} sm={2}>
-           Password
-          </Col>
-          <Col sm={10}>
-            <FormControl
-              type="password"
-              name="password"
-              placeholder="Password"
-              onChange={this.handleChange}
-              value={this.state.password}
-            />
-          </Col>
-        </FormGroup>
+        <Form horizontal onSubmit={this.signup}>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}>
+             Email
+            </Col>
+            <Col sm={10}>
+              <FormControl
+                type="text"
+                placeholder="Email"
+                name="email"
+                onChange={this.handleChange}
+                value={this.state.email}
+                required
+              />
+            </Col>
+          </FormGroup>
 
-        <FormGroup>
-          <Col smOffset={2} sm={10}>
-            <Button onClick={this.signup}>
-             Sign up
-           </Button>
-          </Col>
-        </FormGroup>
-      </Form>
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}>
+             Password
+            </Col>
+            <Col sm={10}>
+              <FormControl
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={this.state.password}
+                required
+              />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit" >
+               Sign up
+             </Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
     );
   }
 }

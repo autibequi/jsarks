a2e3133c363d655b7eb8f8b9700a1453c1b6ead8
Pedro Correa <pedroorez@gmail.com>
2017-03-29 19:29:57 -0300

    Change brownser history router to hashHistory.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,12 +3,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory, Router } from 'react-router';
+import { hashHistory, Router } from 'react-router';
 import routes from './routes.jsx';
 
 const App = () => (
   <div>
-    <Router history={browserHistory} routes={routes} />
+    <Router history={hashHistory} routes={routes} />
   </div>
 );
 

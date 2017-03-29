c3c88a5b2fbcf4fe7248b399d9495008779abd23
Pedro Correa <pedroorez@gmail.com>
2017-03-29 18:08:57 -0300

    Simplify index.jsx.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,20 +6,10 @@ import { render } from 'react-dom';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes.jsx';
 
-class App extends React.Component {
-  getInitialState() {
-    return {
-      ha: 'ha',
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Router history={browserHistory} routes={routes} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Router history={browserHistory} routes={routes} />
+  </div>
+);
 
 render(<App />, document.body);

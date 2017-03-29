e4511a5bbfeb41913a1a96527b5881dd94edd40f
Pedro Correa <pedroorez@gmail.com>
2017-03-29 18:01:53 -0300

    Adds React Bootstrap.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,6 @@
 /* global document */
 // Libs
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory, Router } from 'react-router';

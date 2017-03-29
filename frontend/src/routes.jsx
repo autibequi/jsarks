820e2042e33274c247e81d77e27d60031305622a
Pedro Correa <pedroorez@gmail.com>
2017-03-29 18:02:31 -0300

    Adds Router templates.

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,7 +1,47 @@
 import Base from './containers/Base.jsx';
+import LoginPage from './containers/Login.jsx';
+import HomePage from './containers/Home.jsx';
+import SignUpPage from './containers/SignUp.jsx';
+import Auth from './modules/Auth.jsx';
+import UserPage from './containers/User.jsx';
+
 const routes = {
   // base component (wrapper for the whole application).
   component: Base,
+  childRoutes: [
+    {
+      path: '/',
+      getComponent: (location, callback) => {
+        if (Auth.isUserAuthenticated()) {
+          callback(null, HomePage);
+        } else {
+          callback(null, SignUpPage);
+        }
+      },
+    },
+
+    {
+      path: '/login',
+      component: LoginPage,
+    },
+
+    {
+      path: '/user',
+      component: UserPage,
+    },
+
+    {
+      path: '/signup',
+      component: SignUpPage,
+    },
+
+    {
+      path: '/logout',
+      onEnter: (nextState, replace) => {
+        Auth.deauthenticateUser();
+        replace('/');
+      },
+    },
   ],
 };
 
